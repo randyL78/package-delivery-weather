3e8580921c2a20a93392be3f8bb9f27a93af279d
Randy's Mac <randydavidl78@gmail.com>
2019-01-07 08:35:23 -0500

    add more packages routes

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -12,16 +12,41 @@ router.get('/', (req, res) => {
 });
 
 /* routes for handling database connections */
+/* **************************************** */
+
+// get the list of packages
 router.get('/packages', (req, res) => {
   res.send(`<h1>Getting saved packages from DB</h1>`);
 });
 
+// add a new package to the list
+router.post('/packages', (req, res) => {
+  res.send(`<h1>Adding package to DB</h1>`);
+});
+
+// make a change to a package
+router.put('/packages/:trackingId', (req, res) => {
+  res.send(`<h1>Making a change to package with tracking number: ${req.params.trackingId}</h1>`);
+});
+
+// delete a package
+router.delete('/packages/:trackingId', (req, res) => {
+  res.send(`<h1>Deleting package with tracking number: ${req.params.trackingId}</h1>`);
+});
+
+
 /* routes for Ups API */
+/* ****************** */
+
+// get tracking information for a single package using UPS
 router.get('/tracking', (req, res) => {
   res.send(`<h1>Getting delivery information</h1>`);
 });
 
 /* routes for Weather APU */
+/* ********************** */
+
+// get the delivery weather for a single package, based on tracking #
 router.get('/weather', (req, res) => {
   res.send(`<h1>Getting weather data</h1>`);
 });
